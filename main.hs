4fce33b7771c5bc875d3348699eee186db2cd6ee
MrHencke <henckedev@outlook.com>
2021-11-02 18:26:51 +0100

    Final V3, cant be bothered

--- main.hs
+++ main.hs
@@ -1,16 +1,19 @@
-import GHC.Unicode (isDigit)
+module Main where
+
+import Data.Char (isDigit)
 
 type Board = [[Int]]
 
-type MSG = (String, String)
-
 type State = [Board]
 
-main :: IO ()
-main = gameLoop [] [] 0 ""
+type MSG = (String, String) -- Color code + message
 
-gameLoop :: Board -> State -> Int -> String -> IO ()
+main :: IO ()
+main = gameLoop [] [] 0 ("R", "")
+
+gameLoop :: Board -> State -> Int -> MSG -> IO ()
 gameLoop [] state nm msg = do
+  -- Initial game loop, a player can either start a game or quit from here
   clr
   titlecard
   putStrLn "To list all available commands, use the: \"help\" command"
@@ -22,14 +25,16 @@ gameLoop [] state nm msg = do
     ["help"] -> do
       clr
       help
-      gameLoop [] state nm ""
+      gameLoop [] state nm ("R", "")
     ["q"] -> return ()
-    _ -> gameLoop [] state nm "You have to initialize the game to input other commands"
+    _ -> gameLoop [] state nm ("R", "You have to initialize the game to input other commands")
 gameLoop [[], [], xs] _ nm _ = do
+  -- Game loop when a player has won, they may return to the initial loop from here
   clr
   winMessage xs nm
-  gameLoop [] [] 0 "You've won, but can you do it again?"
+  gameLoop [] [] 0 ("G", "You've won, but can you do it again?")
 gameLoop board state nm msg = do
+  -- Main game loop, where the game is played
   clr
   drawTowers board
   drawMoves nm
@@ -39,18 +44,18 @@ gameLoop board state nm msg = do
     ["b", n] -> newGame board n state nm
     ["z", n] ->
       if checkDigit n
-        then
-          if read n > nm
-            then gameLoop (state !! nm) [] 0 "You can't go back further than your moves \nTaking you back to the starting position"
+        then do
+          let num = read n
+          if num > nm
+            then gameLoop (state !! nm) [state !! nm] 0 ("Y", "You can't go back further than your moves \nTaking you back to the starting position")
             else do
-              let num = read n
-              gameLoop (state !! num) (drop num state) (nm - num) ("Regretting: " ++ n ++ " moves")
-        else gameLoop board state nm "Please input a valid number"
+              gameLoop (state !! num) (drop num state) (nm - num) ("G", "Regretting: " ++ n ++ " moves")
+        else gameLoop board state nm ("R", "Please input a valid number")
     ["help"] -> do
       clr
       help
-      gameLoop board state nm ""
-    ["h"] -> gameLoop board state nm "Yet to be implemented, dont hold your breath"
+      gameLoop board state nm ("R", "")
+    ["h"] -> gameLoop board state nm ("Y", "Yet to be implemented, dont hold your breath")
     ["q"] -> return ()
     [f, t] ->
       if checkDigit f && checkDigit t
@@ -58,12 +63,12 @@ gameLoop board state nm msg = do
           if legalMove board (read f) (read t)
             then do
               let newBoard = move board (read f) (read t)
-              gameLoop newBoard (newBoard : state) (nm + 1) ""
-            else gameLoop board state nm "That move is illegal"
-        else gameLoop board state nm "Please input a valid number"
-    xs -> gameLoop board state nm ("\"" ++ unwords xs ++ "\"" ++ " is not a valid command, check available commands with \"help\"")
+              gameLoop newBoard (newBoard : state) (nm + 1) ("R", "")
+            else gameLoop board state nm ("R", "That move is illegal")
+        else gameLoop board state nm ("R", "Please input a valid number")
+    xs -> gameLoop board state nm ("R", "\"" ++ unwords xs ++ "\"" ++ " is not a valid command, check available commands with \"help\"")
 
----------------Hjelpemetoder---------------
+--------------- Helper functions ---------------
 
 move :: Board -> Int -> Int -> Board -- Cant really think of a dynamic way to do this without making spaghetti code
 move [a : as, b, c] 1 2 = [as, a : b, c]
@@ -72,17 +77,16 @@ move [a, b : bs, c] 2 3 = [a, bs, b : c]
 move [a, b : bs, c] 2 1 = [b : a, bs, c]
 move [a, b, c : cs] 3 1 = [c : a, b, cs]
 move [a, b, c : cs] 3 2 = [a, c : b, cs]
-move board _ _ = board
 
 help :: IO ()
 help = do
-  putStrLn "\nThere are 4 commands:"
+  putStrLn "There are 4 commands:"
   putStrLn "b <number of rings>: Starts a new game with a given number of rings"
-  putStrLn "q: Quits the game, losing all state"
   putStrLn "<f> <t>: Moves a ring from pole f to pole t, if the move is legal"
   putStrLn "z <n>: Regrets n moves"
-  putStrLn "h: This command is supposed to give you a hint\n"
-  _ <- promptLine "Enter any key to return to the game"
+  putStrLn "h: This command is supposed to give you a hint"
+  putStrLn "q: Quits the game, losing all state\n"
+  _ <- promptLine "Press enter to return to the game"
   return ()
 
 initializeBoard :: (Num a, Enum a) => a -> [[a]]
@@ -94,8 +98,7 @@ drawTowers [t1, t2, t3] = do
   writeRows (2 + mh) (reverse t1) mh
   writeRows (3 * (2 + mh)) (reverse t2) mh
   writeRows (5 * (2 + mh)) (reverse t3) mh
-  goto 0 (mh + 2)
-drawTowers _ = return ()
+  goto 0 (mh + 2) -- Return line under towers
 
 writeRows :: Int -> [Int] -> Int -> IO ()
 writeRows _ _ 0 = return ()
@@ -104,7 +107,7 @@ writeRows pivot [] mh = do
   writeRows pivot [] (mh - 1)
 writeRows pivot n mh = do
   let el = head n
-  writeAt " #" el (pivot - el) mh --Write "#"
+  writeAt " #" el (pivot - el) mh
   writeRows pivot (tail n) (mh - 1)
 
 writeAt :: String -> Int -> Int -> Int -> IO ()
@@ -115,8 +118,13 @@ writeAt str n pivot height = do
 drawMoves :: Int -> IO ()
 drawMoves nm = putStrLn ("Number of moves: " ++ show nm)
 
-drawMessage :: String -> IO ()
-drawMessage msg = if msg == "" then putStr "\n" else putStr ("\ESC[31m" ++ msg ++ "\ESC[0m\n")
+drawMessage :: MSG -> IO ()
+drawMessage (color, msg) = if msg == "" then putStr "\n" else putStr ("\ESC[" ++ colorToCode color ++ "m" ++ msg ++ "\ESC[0m\n")
+
+colorToCode :: String -> String
+colorToCode "R" = "31"
+colorToCode "G" = "32"
+colorToCode "Y" = "33"
 
 legalMove :: Board -> Int -> Int -> Bool
 legalMove board x y =
@@ -135,69 +143,52 @@ newGame board n state nm =
   if checkDigit n
     then do
       let num = read n :: Int
-      if num > 12
-        then gameLoop board state nm "Right now, 12 is the maximum amount of rings playable."
-        else do
+      if 0 < num && num <= 12
+        then do
           let newBoard = initializeBoard num
-          gameLoop newBoard [newBoard] 0 ("A game with " ++ n ++ " rings will at the very least take " ++ show (2 ^ num - 1 :: Int) ++ " moves.")
-    else gameLoop board state nm "Please input a valid number"
+          gameLoop newBoard [newBoard] 0 ("G", "A game with " ++ n ++ " rings will at the very least take " ++ show (2 ^ num - 1 :: Int) ++ " moves. Good luck.")
+        else gameLoop board state nm ("R", "Right now, 12 is the maximum amount of rings playable.")
+    else gameLoop board state nm ("R", "Please input a valid number")
 
 winMessage :: [Int] -> Int -> IO ()
 winMessage xs nm = do
-  clr
   putStrLn "You won, congratulations!"
   let rings = last xs
   let optimalSol = 2 ^ rings - 1 :: Int
   putStrLn ("This game contained " ++ show rings ++ " rings")
-  let message =
-        "You used " ++ show nm
-          ++ if optimalSol == nm
-            then " moves, just like the optimal solution. Good job!"
-            else " moves, while the optimal solution uses " ++ show optimalSol ++ ". Better luck next time!"
-  putStrLn message
+  putStrLn
+    ( "You used " ++ show nm
+        ++ if optimalSol == nm
+          then " moves, just like the optimal solution. Good job!"
+          else " moves, while the optimal solution uses " ++ show optimalSol ++ ". Better luck next time!"
+    )
+
   _ <- promptLine "Press enter to continue to the main menu"
   return ()
 
 ------------- Utility functions -------------
 
 clr :: IO ()
-clr = do
-  putStr "\ESC[2J"
-  goto 0 0
+clr = putStr "\ESC[2J" >> goto 0 0
 
 goto :: Int -> Int -> IO ()
 goto x y = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
 
 promptLine :: String -> IO String
-promptLine [] = do
-  putStr "> "
-  getLine
-promptLine prompt = do
-  putStrLn prompt
-  putStr "> "
-  getLine
+promptLine [] = putStr "> " >> getLine
+promptLine prompt = putStrLn prompt >> putStr "> " >> getLine
 
 checkDigit :: String -> Bool
 checkDigit = all isDigit
 
+------------- ASCII Art -------------
+
 titlecard :: IO ()
 titlecard =
   putStrLn
-    " ████████╗ █████╗ ██╗       ██╗███████╗██████╗  ██████╗    █████╗ ███████╗  ██╗  ██╗ █████╗ ███╗  ██╗ █████╗ ██╗\n \
-    \╚══██╔══╝██╔══██╗██║  ██╗  ██║██╔════╝██╔══██╗██╔════╝   ██╔══██╗██╔════╝  ██║  ██║██╔══██╗████╗ ██║██╔══██╗██║\n \
-    \   ██║   ██║  ██║╚██╗████╗██╔╝█████╗  ██████╔╝╚█████╗    ██║  ██║█████╗    ███████║███████║██╔██╗██║██║  ██║██║\n \
-    \   ██║   ██║  ██║ ████╔═████║ ██╔══╝  ██╔══██╗ ╚═══██╗   ██║  ██║██╔══╝    ██╔══██║██╔══██║██║╚████║██║  ██║██║\n \
-    \   ██║   ╚█████╔╝ ╚██╔╝ ╚██╔╝ ███████╗██║  ██║██████╔╝   ╚█████╔╝██║       ██║  ██║██║  ██║██║ ╚███║╚█████╔╝██║\n \
+    "████████╗ █████╗ ██╗       ██╗███████╗██████╗  ██████╗    █████╗ ███████╗  ██╗  ██╗ █████╗ ███╗  ██╗ █████╗ ██╗\n\
+    \╚══██╔══╝██╔══██╗██║  ██╗  ██║██╔════╝██╔══██╗██╔════╝   ██╔══██╗██╔════╝  ██║  ██║██╔══██╗████╗ ██║██╔══██╗██║\n\
+    \   ██║   ██║  ██║╚██╗████╗██╔╝█████╗  ██████╔╝╚█████╗    ██║  ██║█████╗    ███████║███████║██╔██╗██║██║  ██║██║\n\
+    \   ██║   ██║  ██║ ████╔═████║ ██╔══╝  ██╔══██╗ ╚═══██╗   ██║  ██║██╔══╝    ██╔══██║██╔══██║██║╚████║██║  ██║██║\n\
+    \   ██║   ╚█████╔╝ ╚██╔╝ ╚██╔╝ ███████╗██║  ██║██████╔╝   ╚█████╔╝██║       ██║  ██║██║  ██║██║ ╚███║╚█████╔╝██║\n\
     \   ╚═╝    ╚════╝   ╚═╝   ╚═╝  ╚══════╝╚═╝  ╚═╝╚═════╝     ╚════╝ ╚═╝       ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝ ╚════╝ ╚═╝\n"
-
-testHanoi :: Board
-testHanoi = [[1, 2, 3], [], []]
-
-hS :: Board -> Int -> State -> State
-hS board 0 akk = akk -- base case
-hS board n akk
-  | n `elem` c = if (n - 1) `elem` b then hS (move board 2 3) (n - 1) ((move board 2 3) : akk) else hS (move board 1 3) (n - 1) ((move board 1 3) : akk) --rek case 1
-  | n `elem` b = if (n - 1) `elem` c then hS (move board 3 1) (n - 1) ((move board 3 1) : akk) else hS board n akk -- flytt (n-1) til a, flytt n til c, flytt (n-1) til c
-  | n `elem` a = if (n - 1) `elem` c then hS (move board 3 2) (n - 1) ((move board 3 2) : akk) else hS board n akk -- flytt (n-1) til b, flytt n til c, flytt (n-1) til c
-  where
-    [a, b, c] = board
